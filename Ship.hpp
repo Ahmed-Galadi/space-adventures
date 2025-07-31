1568844c6d32ed4c24fd2254ce557ea1c0092c29
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-07-31 07:22:10 +0100

    making the first class in this bitsh

---
+++ Ship.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Ship {
+	protected:
+		std::string		name;
+		unsigned int	health;
+		unsigned int	power;
+		unsigned int	defence;
+		unsigned int 	luck;
+		int 			level;
+		
+};
