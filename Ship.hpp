8bd3509c7586f04bedd599c6ad44c77c5cced562
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-08-01 12:19:29 +0100

    making the Ship.hpp file for starters .. lets see where this is going

--- Ship.hpp
+++ Ship.hpp
@@ -9,6 +9,26 @@ class Ship {
 		unsigned int	power;
 		unsigned int	defence;
 		unsigned int 	luck;
+		unsigned int	speed;
 		int 			level;
-		
+	
+	public:
+		Ship();
+		Ship(const std::string &name);
+		bool 	operator>(const Ship &other);
+		bool 	operator<(const Ship &other);
+		~Ship();
+
+		virtual void	levelUp(int amount) = 0;
+		void			getDamage(const Ship &other);
+		void 			attack(const Ship &other);
+		void 			deffend(const Ship &other);
+		void 			healthUp(int amount);
+		void 			powerUp(int amount);
+		void			speedUp(int amount);
+		void			getLucky(int amount, int time);
+		void 			getInvisible(int time);
+		void			godMode(int time);
+		void			getCursed(int time);
+		void			getPoisened(int time, int ammount);
 };
